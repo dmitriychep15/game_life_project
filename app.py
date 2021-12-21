0db39079d5e567fe49bf6f59d2d389ed0f07a338
Chep <dimdemz@mail.ru>
2021-12-22 02:40:05 +0300

    Init. Game works, script part was removed to make debugging easier (this part is in hui.py)

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from game_of_life import *
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
+@app.route('/')
+def home():
+    GameOfLife(25, 25)
+    return render_template('index.html')
+
+@app.route('/live/')
+def live():
+    gen = GameOfLife()
+    gen.form_new_generation()
+    gen.counter += 1
+    return render_template('live.html', gen=gen)
+
---
+++ hui.py
@@ -0,0 +1,10 @@
+from game_of_life import *
+
+a = GameOfLife()
+
+print(dir(a))
+print(a[2][3])
+
+<script>
+    setInterval( () => window.location.reload(), 1000);
+</script>
